87e6c95520fc67808060f9aac1f449fd05e6bd0b
Wojtek Mach <wojtek@wojtekmach.pl>
2021-02-11 11:04:58 +0100

    Handle exponents

--- lib/unit.ex
+++ lib/unit.ex
@@ -16,42 +16,12 @@ defmodule Unit do
       iex> Unit.eval 120[km] / 2[h]
       #Unit<60.0[km/h]>
 
+      iex> Unit.eval 10[m] * 20[m]
+      #Unit<200[m2]>
+
   """
   defmacro eval(do: block), do: do_eval(block)
   defmacro eval(block), do: do_eval(block)
-
-  defp do_eval(block) do
-    block =
-      Macro.prewalk(block, fn
-        {{:., _, [Access, :get]}, _, [magnitude, unit]} ->
-          Macro.escape(%__MODULE__{magnitude: magnitude, unit: unit(unit)})
-
-        other ->
-          other
-      end)
-
-    quote do
-      # `if` adds scope and we need it so that imports don't leak outside
-      if true do
-        import Kernel, except: [+: 2, -: 2, *: 2, /: 2]
-        import Unit.Kernel
-        unquote(block)
-      end
-    end
-  end
-
-  defp unit(ast) do
-    Macro.postwalk(ast, fn
-      {unit, _, nil} ->
-        unit
-
-      {:/, _, ast} ->
-        {:/, ast}
-
-      {:*, _, ast} ->
-        {:*, ast}
-    end)
-  end
 
   @doc """
   Adds two values.
@@ -79,7 +49,7 @@ defmodule Unit do
   Multiplies two values.
   """
   def mult(%__MODULE__{magnitude: mag1, unit: unit1}, %__MODULE__{magnitude: mag2, unit: unit2}) do
-    %__MODULE__{magnitude: mag1 * mag2, unit: {:*, [unit1, unit2]}}
+    %__MODULE__{magnitude: mag1 * mag2, unit: normalize_unit({:*, [unit1, unit2]})}
   end
 
   def mult(%__MODULE__{magnitude: mag, unit: unit}, number) when is_number(number) do
@@ -90,29 +60,96 @@ defmodule Unit do
   Divides two values.
   """
   def div(%__MODULE__{magnitude: mag1, unit: unit1}, %__MODULE__{magnitude: mag2, unit: unit2}) do
-    %__MODULE__{magnitude: mag1 / mag2, unit: {:/, [unit1, unit2]}}
+    %__MODULE__{magnitude: mag1 / mag2, unit: normalize_unit({:/, [unit1, unit2]})}
   end
 
   def div(%__MODULE__{magnitude: mag, unit: unit}, number) when is_number(number) do
     %__MODULE__{magnitude: mag / number, unit: unit}
   end
 
-  defimpl Inspect do
-    def inspect(%{magnitude: magnitude, unit: unit}, _) do
-      "#Unit<#{magnitude}[#{unit(unit)}]>"
-    end
+  defp do_eval(block) do
+    block =
+      Macro.prewalk(block, fn
+        {{:., _, [Access, :get]}, _, [magnitude, unit]} ->
+          Macro.escape(%__MODULE__{magnitude: magnitude, unit: unit(unit)})
 
-    defp unit({:/, [a, b]}) do
-      unit(a) <> "/" <> unit(b)
-    end
+        other ->
+          other
+      end)
 
-    defp unit({:*, [a, b]}) do
-      unit(a) <> " " <> unit(b)
+    quote do
+      # `if` adds scope and we need it so that imports don't leak outside
+      if true do
+        import Kernel, except: [+: 2, -: 2, *: 2, /: 2]
+        import Unit.Kernel
+        unquote(block)
+      end
     end
+  end
 
-    defp unit(atom) when is_atom(atom) do
-      Atom.to_string(atom)
+  defp unit({:*, _, list}) do
+    normalize_unit({:*, Enum.map(list, &unit/1)})
+  end
+
+  defp unit({:/, _, list}) do
+    normalize_unit({:/, Enum.map(list, &unit/1)})
+  end
+
+  defp unit({atom, _, nil}) when is_atom(atom) do
+    case atom |> Atom.to_string() |> String.reverse() |> Integer.parse() do
+      {int, str} ->
+        {str |> String.reverse() |> String.to_atom(),
+         int |> Integer.digits() |> Enum.reverse() |> Integer.undigits()}
+
+      :error ->
+        {atom, 1}
     end
+  end
+
+  defp normalize_unit({:*, [left, right]}) do
+    case {normalize_unit(left), normalize_unit(right)} do
+      {{unit, exp1}, {unit, exp2}} ->
+        {unit, exp1 + exp2}
+
+      {left, right} ->
+        {:*, [left, right]}
+    end
+  end
+
+  defp normalize_unit({:/, [left, right]}) do
+    case {normalize_unit(left), normalize_unit(right)} do
+      {{unit, exp1}, {unit, exp2}} ->
+        {unit, exp1 - exp2}
+
+      {left, right} ->
+        {:/, [left, right]}
+    end
+  end
+
+  defp normalize_unit(other) do
+    other
+  end
+end
+
+defimpl Inspect, for: Unit do
+  def inspect(%{magnitude: magnitude, unit: unit}, _) do
+    "#Unit<#{magnitude}[#{unit(unit)}]>"
+  end
+
+  defp unit({:*, list}) do
+    Enum.map_join(list, "*", &unit/1)
+  end
+
+  defp unit({:/, [left, right]}) do
+    unit(left) <> "/" <> unit(right)
+  end
+
+  defp unit({unit, 1}) do
+    Atom.to_string(unit)
+  end
+
+  defp unit({unit, exp}) do
+    Atom.to_string(unit) <> Integer.to_string(exp)
   end
 end
 
--- test/unit_test.exs
+++ test/unit_test.exs
@@ -1,4 +1,23 @@
 defmodule UnitTest do
   use ExUnit.Case, async: true
   doctest Unit
+  require Unit
+
+  test "eval" do
+    assert Unit.eval(1[m]) == %Unit{magnitude: 1, unit: {:m, 1}}
+    assert Unit.eval(1[m2]) == %Unit{magnitude: 1, unit: {:m, 2}}
+    assert Unit.eval(1[m * m]) == %Unit{magnitude: 1, unit: {:m, 2}}
+    assert Unit.eval(1[m * m * m]) == %Unit{magnitude: 1, unit: {:m, 3}}
+    assert Unit.eval(1[m * m * s]) == %Unit{magnitude: 1, unit: {:*, [{:m, 2}, {:s, 1}]}}
+    assert Unit.eval(1[m / s]) == %Unit{magnitude: 1, unit: {:/, [{:m, 1}, {:s, 1}]}}
+    assert Unit.eval(1[m / s2]) == %Unit{magnitude: 1, unit: {:/, [{:m, 1}, {:s, 2}]}}
+    assert Unit.eval(1[m2 / m]) == %Unit{magnitude: 1, unit: {:m, 1}}
+
+    assert inspect(Unit.eval(1[m])) == "#Unit<1[m]>"
+    assert inspect(Unit.eval(1[m2])) == "#Unit<1[m2]>"
+    assert inspect(Unit.eval(1[m * s])) == "#Unit<1[m*s]>"
+    assert inspect(Unit.eval(1[m / s])) == "#Unit<1[m/s]>"
+
+    assert Unit.eval(1[m] * 1[s]) == Unit.eval(1[m * s])
+  end
 end
